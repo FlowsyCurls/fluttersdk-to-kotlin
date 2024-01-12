7d7005d59c161e3db4ea91293fbbed0b57ee2fc8
Shakime Sparks <s.jeykime@gmail.com>
2024-01-12 13:30:30 -0600

    add kotlin demo without flutter

--- flutter_module/lib/main.dart
+++ flutter_module/lib/main.dart
@@ -1,72 +1,20 @@
 import 'package:flutter/services.dart';
-import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
+  const MethodChannel channel = MethodChannel('sum_channel');
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+  channel.setMethodCallHandler((MethodCall call) async {
+    if (call.method == 'computeSum') {
+      int a = call.arguments['a'];
+      int b = call.arguments['b'];
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String _counter = '';
-  static const platform = MethodChannel('example.com/channel');
-  Future<void> _generateRandomString() async {
-    String random = '';
-    try {
-      random = await platform.invokeMethod('getRandomString', 5);
-    } on PlatformException catch (e) {
-      random = '';
-      print('PlatformException: ${e.code} ${e.message}');
+      int result = computeSum(a, b);
+      return result;
     }
-    setState(() {
-      _counter = random;
-    });
-  }
+    return null;
+  });
+}
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Kotlin generates the following string:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _generateRandomString,
-        tooltip: 'Generate',
-        child: const Icon(Icons.refresh),
-      ),
-    );
-  }
+int computeSum(int a, int b) {
+  return a + b;
 }
--- flutter_module/lib/methods.dart
+++ flutter_module/lib/methods.dart
@@ -1,39 +1,16 @@
 import 'package:flutter/services.dart';
 
-class Calculator {
-  static const MethodChannel _channel =
-      MethodChannel('samples.flutter.dev/calculator');
+class MethodChannelHelper {
+  static const platform = const MethodChannel('com.example.sum_channel');
 
-  static void setup() {
-    _channel.setMethodCallHandler((MethodCall call) async {
-      if (call.method == "add") {
-        final a = call.arguments['a'] as int;
-        final b = call.arguments['b'] as int;
-        return add(a, b);
-      } else if (call.method == "mult") {
-        final a = call.arguments['a'] as int;
-        final b = call.arguments['b'] as int;
-        return mult(a, b);
-      } else if (call.method == "div") {
-        final a = call.arguments['a'] as int;
-        final b = call.arguments['b'] as int;
-        return div(a, b);
-      }
-    });
-  }
-
-  static int add(int a, int b) {
-    return a + b;
-  }
-
-  static int mult(int a, int b) {
-    return a * b;
-  }
-
-  static double div(int a, int b) {
-    if (b == 0) {
-      throw Exception("Cannot divide by zero");
+  static Future<int> calculateSum(int a, int b) async {
+    try {
+      final result =
+          await platform.invokeMethod('calculateSum', {'a': a, 'b': b});
+      return result;
+    } on PlatformException catch (e) {
+      print("Error: ${e.message}");
+      return 0; // or some default value
     }
-    return a / b;
   }
 }
